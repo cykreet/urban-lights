d9df938c06ac1f4f11eb409244ddeccf714e7ede
cykreet <cykreet@gmail.com>
2025-06-15 13:37:13 +0200

    feat: uncoloured versions used for colour theme

--- scripts/prepare-icons.sh
+++ scripts/prepare-icons.sh
@@ -1,8 +1,25 @@
-#! /bin/bash
+#!/bin/bash
+# automatically converts icons in the normal icon theme to coloured versions found in the svg folder
+
+# these icons don't have coloured versions, and should therefore just use the
+# non-coloured versions
+# example: solidity-icon.svg -> solidity
+exclusion_list="solidity,sql"
 
 icons_path="$(pwd)/icons"
+IFS=',' read -ra excluded_array <<< "$exclusion_list"
 
-json=$(cat $icons_path/urban-lights-icon-theme.json)
-# Replace all "./svg/word-icon.svg" strings with the actual word and append "-active" to "icon"
-icon_colours_json=$(echo "$json" | sed 's/"\.\/svg\/\([a-z0-9_]*\)-icon\.svg"/"\.\/svg\/\1-icon-active.svg"/g')
-> $icons_path/urban-lights-icon-theme-colours.json echo "$icon_colours_json"
+json=$(cat "$icons_path/urban-lights-icon-theme.json")
+
+sed_cmd=""
+for excluded in "${excluded_array[@]}"; do
+	sed_cmd="/$excluded/!{$sed_cmd"
+done
+
+sed_cmd="${sed_cmd}s/-icon\.svg\"/-icon-active.svg\"/g"
+for excluded in "${excluded_array[@]}"; do
+	sed_cmd="${sed_cmd}}"
+done
+
+result=$(echo "$json" | sed "$sed_cmd")
+echo "$result" > "$icons_path/urban-lights-icon-theme-colours.json"
